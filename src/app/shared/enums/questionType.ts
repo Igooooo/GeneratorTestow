44f07e29d514622e86537d979dcdf3c023df5fec
Igor Nalepka <nalepkaigor@gmail.com>
2021-08-18 14:53:13 +0200

    fix(IN): new type in questionType

--- src/app/shared/enums/questionType.ts
+++ src/app/shared/enums/questionType.ts
@@ -1,5 +1,12 @@
 export enum questionType {
     all = 'wszystkie',
-    q1 = 'Kosz 1',
-    q2 = 'Kosz 2'
+    q1 = 'Funkcja liniowa',
+    q2 = 'Procenty',
+    q3 = 'Trygonometria',
+    q4 = 'Geometria analityczna',
+    q5 = 'Równania i nierówności kwadratowe',
+    q6 = 'Potęgi pierwiastki logarytmy',
+    q7 = 'Ciągi',
+    q8 = 'Statystyka',
+    q9 = 'Prawdopodobieństwo',
 }
